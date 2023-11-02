c5d62299cda81f1dca4bb304cf8b79e214f996f0
verystrongvery <jinbkim42@gmail.com>
2023-11-02 12:26:01 +0900

    feat: Spring Boot 프로젝트 초기설정

---
+++ websocket_practice_backend/src/test/java/com/example/websocket_practice_backend/WebsocketPracticeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_practice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketPracticeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
